4ad6abbd643b9d12a4970f051f5ba6c56d710e47
cassiogroh <cassio_groh@hotmail.com>
2020-10-14 00:04:53 -0300

    Android deploy config

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -1,22 +1,29 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle='light-content' backgroundColor='#312E38' translucent />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#312E38' }} >
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle='light-content' backgroundColor='#312E38' translucent />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312E38' }} >
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  )
+};
 
 export default App;
